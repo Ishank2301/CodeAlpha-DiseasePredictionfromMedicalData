1ee59c2f4b99b0c864c6813542c7a47e33885b78
Ishank <ishankmishra579@gmil.com>
2026-06-02 13:59:27 +0530

    chore: preprocess only best_model.pkl

---
+++ src/preprocess.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+FEATURES = [
+    "age",
+    "sex",
+    "cp",
+    "trestbps",
+    "chol",
+    "fbs",
+    "restecg",
+    "thalach",
+    "exang",
+    "oldpeak",
+    "slope",
+    "ca",
+    "thal",
+]
+
+
+def preprocess_input(data):
+
+    df = pd.DataFrame([data])
+
+    return df[FEATURES]
